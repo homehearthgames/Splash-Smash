8ff392dda942b9b0c198f4a58795ab83ed9b270c
Christopher Driscoll <cdrisco@users.noreply.github.com>
2023-02-25 16:20:40 -0600

    added splash and BG wave layers

--- Splash&Smash/Assets/Scripts/PlayerScript.cs
+++ Splash&Smash/Assets/Scripts/PlayerScript.cs
@@ -15,6 +15,9 @@ public class PlayerScript : MonoBehaviour
     public ParticleSystem splash1;
     public ParticleSystem splash2;
     public ParticleSystem splash3;
+    public ParticleSystem splash4;
+
+    public GameObject splash;
 
     // Start is called before the first frame update
     void Start()
@@ -46,15 +49,42 @@ public class PlayerScript : MonoBehaviour
             seq.append(() => { // fire event after tween
                 Debug.Log("Landed");
                 isJumping = false;
+                ResetSplash();
+                float mult = 1.6f;
+                splash1.startSpeed = waveHeight * 3 + 2;
+                splash2.startSpeed = waveHeight * 3 + 2;
+                splash3.startSpeed = waveHeight * 3 + 2;
+
+                splash1.maxParticles = (int)(waveHeight * 150);
+                splash2.maxParticles = (int)(waveHeight * 150);
+                splash3.maxParticles = (int)(waveHeight * 150);
+                //transform.localScale = new Vector3(waveHeight*mult, waveHeight * mult, 1);
                 splash1.Play();
                 splash2.Play();
                 splash3.Play();
+                splash4.Play();
             }); ;
+            seq.append(LeanTween.moveLocalY(gameObject, -1, 1).setEase(LeanTweenType.easeOutExpo)); // dip down
+            seq.append(LeanTween.moveLocalY(gameObject, 0, 1).setEase(LeanTweenType.easeOutBounce)); // dip up
+
 
             Debug.Log("Wave size:" + waveHeight+" jump height: "+ (jumpHeight * waveHeight));
         }
     }
 
+    void ResetSplash()
+    {
+        StartCoroutine(RSplash());
+    }
+
+    IEnumerator RSplash()
+    {
+        while(!splash1.isEmitting)
+        {
+            yield return null;
+        }
+        splash.transform.position = transform.position;
+    }
     void Jumping()
     {
         
---
+++ Splash&Smash/Assets/WaveBGScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveBGScript : MonoBehaviour
+{
+    public float waveSpeed = 0;
+    public float waveAccelleration;
+    public float maxWaveSpeed;
+    public GameObject waveBG1;
+    public GameObject waveBG2;
+    public GameObject splash;
+    private GameObject player;
+
+    public int waveWidth;
+
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waveSpeed += waveAccelleration * Time.deltaTime;
+        if (waveSpeed > maxWaveSpeed)
+            waveSpeed = maxWaveSpeed;
+
+        // scroll waves
+        if (waveBG1.transform.position.x < waveBG2.transform.position.x)
+        {
+            waveBG1.transform.position = new Vector3(waveBG1.transform.position.x - waveSpeed, waveBG1.transform.position.y, waveBG1.transform.position.z);
+            waveBG2.transform.position = new Vector3(waveBG1.transform.position.x + waveWidth, waveBG2.transform.position.y, waveBG2.transform.position.z);
+        }
+        else
+        {
+            waveBG2.transform.position = new Vector3(waveBG2.transform.position.x - waveSpeed, waveBG2.transform.position.y, waveBG2.transform.position.z);
+            waveBG1.transform.position = new Vector3(waveBG2.transform.position.x + waveWidth, waveBG1.transform.position.y, waveBG1.transform.position.z);
+        }
+
+
+        if (waveBG1.transform.position.x <= -waveWidth)
+        {
+            waveBG1.transform.position = new Vector3(waveBG2.transform.position.x + waveWidth, waveBG1.transform.position.y, waveBG1.transform.position.z);
+        }
+
+        if (waveBG2.transform.position.x <= -waveWidth)
+        {
+            waveBG2.transform.position = new Vector3(waveBG1.transform.position.x + waveWidth, waveBG2.transform.position.y, waveBG2.transform.position.z);
+        }
+
+        // move splash wil wave bg
+        if (splash != null && player.GetComponentInChildren<PlayerScript>().isJumping==false && player.GetComponentInChildren<PlayerScript>().splash1.isEmitting)
+            splash.transform.position = new Vector3(splash.transform.position.x - waveSpeed/3,splash.transform.position.y,splash.transform.position.z);
+
+
+    }
+}
